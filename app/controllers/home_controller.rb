3a318fb9b481e2144ac24dd04d7fb6726c7f4080
Amy Lee <lsj220@likelion.org>
2017-04-07 09:34:54 +0000

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+  def index
+    @numbers = (1..45).to_a.sample(6).sort.reverse
+  end
+
+  def likelion
+    @city = 'Brisbane'
+  end
+  
+  def library
+    @qut = 'QUT'
+end
